3408a8494c0f617c153405981814f323a197a788
Cathy <Cathy347Le@gmail.com>
2019-02-26 16:22:50 -0500

    Install and require dependencies, create and test a route, and create a port for server to listen for requests

---
+++ master.js
@@ -0,0 +1,19 @@
+//Install express and create and instance of express
+//Require dependencies
+//Apply dependencies to instance of express
+//Create a port for server to listen to request
+
+const express = require("express");
+const app = express();
+const parser = require("body-parser");
+const methodOverride = require("method-override");
+
+app.set("view engine", "hbs");
+app.use(parser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
+
+app.listen(7000, () => console.log("listening on port 7000"));
---
+++ planning.md
@@ -0,0 +1,27 @@
+Steps
+
+# Models
+
+1. List (Title, Description, [Movie])
+
+- Create
+- Read
+- Update
+- Delete
+
+2. Item (Title, Genre, Imdb rating)
+
+- Create
+- Read
+- Delete
+
+Steps
+
+- Npm init -y
+- Install dependencies
+- Add node_modules to .gitignore
+- require Express, hbs, body-parser, methodOverride, mongoose
+- Send Hello World to the homepage
+- create a port
+- run nodemom
+- commit
