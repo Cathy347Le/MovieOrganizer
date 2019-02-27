3bff817b0baa935d410ca167b5b37cb1dfbb0476
Cathy <Cathy347Le@gmail.com>
2019-02-27 15:28:58 -0500

    added create and show list feature

--- controllers/list.js
+++ controllers/list.js
@@ -5,10 +5,18 @@ module.exports = {
     res.render("list/new");
   },
   create: (req, res) => {
-    res.redirect("/");
+    const { title, description } = req.body;
+    List.create({
+      title,
+      description
+    }).then(list => {
+      res.redirect(`list/${list.id}`);
+    });
   },
   show: (req, res) => {
-    res.send("show list page");
+    List.findById(req.params.id).then(list => {
+      res.render("list/show", { list });
+    });
   },
   edit: (req, res) => {
     res.send("edit list page");
