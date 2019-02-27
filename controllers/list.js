79280a57c32d316c5f70412c99567802f85833ee
Cathy <Cathy347Le@gmail.com>
2019-02-27 15:21:24 -0500

    added show list feature

--- controllers/list.js
+++ controllers/list.js
@@ -2,7 +2,7 @@ const { List } = require("../models/list");
 
 module.exports = {
   new: (req, res) => {
-    res.send("new list page");
+    res.render("list/new");
   },
   create: (req, res) => {
     res.redirect("/");
