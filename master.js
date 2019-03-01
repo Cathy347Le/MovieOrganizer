1b371338b1bfdeee30db4f7042f4619eef8e2644
Cathy <Cathy347Le@gmail.com>
2019-03-01 12:15:58 -0500

    setup for Heroku deployment

--- master.js
+++ master.js
@@ -17,4 +17,10 @@ app.use(express.static("public"));
 
 app.use(require("./routes/index"));
 
-app.listen(7000, () => console.log("listening on port 7000"));
+// app.listen(7000, () => console.log("listening on port 7000"));
+
+app.set("port", process.env.PORT || 3001);
+
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
